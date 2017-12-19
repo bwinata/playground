77801c3ea0dbe9244ca03d000fecbc7aafcbd5d2
Barry Winata <barry.winata@yahoo.com>
2017-12-19 00:22:09 -0800

    Add linked list functions

--- C/linked_list.c
+++ C/linked_list.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
-typedef struct Node {
+struct Node {
         int val;
         struct Node * next;
         struct Node * prev;
 };
 
-typedef struct LL {
+struct LL {
         unsigned int count;
         struct Node * head;
         struct Node * tail;
@@ -15,9 +16,35 @@ typedef struct LL {
 
 // -----------------------------------------------------------------------------
 
+struct Node * create_node (int val)
+{
+        struct Node * new_node = malloc (sizeof (struct Node));
+        new_node->val = val;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+
+        return new_node;
+}
+
+// -----------------------------------------------------------------------------
+
 struct Node * LL_push (struct LL * list, int val)
 {
+        assert (list != NULL);
+        struct Node * n = create_node (val);
 
+        if (list->head == NULL) {
+                list->head = list->tail = n;
+        } else {
+                struct Node * t_head = list->head;
+                list->head = n;
+                list->head->next = t_head;
+                list->head->next->prev = list->head;
+
+        }
+
+        list->count++;
+        return list->head;
 }
 
 // -----------------------------------------------------------------------------
@@ -31,7 +58,19 @@ struct Node * LL_pop (struct LL * list)
 
 struct Node * LL_queue (struct LL * list, int value)
 {
+        assert (list != NULL);
+        struct Node * n = create_node (value);
 
+        if (list->head == NULL) {
+                list->head = list->tail = n;
+        } else {
+                n->prev = list->tail;
+                list->tail->next = n;
+                list->tail = n;
+        }
+
+        list->count++;
+        return list->tail;
 }
 
 // -----------------------------------------------------------------------------
@@ -52,12 +91,49 @@ struct LL * LL_reverse (struct LL * list)
 
 unsigned int LL_count (struct LL * list)
 {
+        assert (list != NULL);
+        return list->count;
+}
 
+// -----------------------------------------------------------------------------
+
+struct LL * LL_create ()
+{
+        struct LL * list = malloc (sizeof (struct LL));
+
+        list->count = 0;
+        list->head  = NULL;
+        list->tail  = NULL;
+
+        return list;
+}
+
+// -----------------------------------------------------------------------------
+
+void LL_print (struct LL * list)
+{
+        struct Node * conductor = list->head;
+        while (conductor != NULL) {
+                printf ("[%d]->", conductor->val);
+                conductor = conductor->next;
+        }
 }
 
 // -----------------------------------------------------------------------------
 
 int main (void)
 {
+        struct LL * list = LL_create ();
+        assert (list != NULL);
+        assert (LL_push (list, 10) == list->head);
+        assert (LL_push (list, 20) == list->head);
+        assert (LL_push (list, 30) == list->head);
+        assert (LL_queue (list, 100) == list->tail);
+
+
+        LL_print (list);
+
+        assert (LL_count (list) == 4);
+
         return 0;
 }
